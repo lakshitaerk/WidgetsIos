767ee2c7cbdc10401e1cc77986ac73bad27d0fde
Lakshita <lakshita2119@gmail.com>
2024-01-27 17:40:29 +0530

    Large

--- Widgets/WidgetLarge.swift
+++ Widgets/WidgetLarge.swift
@@ -9,6 +9,34 @@ import SwiftUI
 import WidgetKit
 import MapKit
 
+class WidgetViewModel2: ObservableObject {
+    @Published var locations: [Location2] = [
+        Location2(name: "Kolkata", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141), imageName: "person3"),
+        Location2(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076), imageName: "person"),
+        Location2(name: "Amritsar", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076), imageName: "person2")
+    ].shuffled()
+
+    private var timer: Timer?
+
+    init() {
+        startTimer()
+    }
+
+   
+
+    deinit {
+        timer?.invalidate()
+    }
+    func refreshData() {
+           locations.shuffle()
+       }
+    private func startTimer() {
+            timer = Timer.scheduledTimer(withTimeInterval: 300, repeats: true) { [weak self] _ in
+                self?.locations.shuffle()
+                MyProvider3().reloadTimeline()
+            }
+        }
+}
 
 
 struct WidgetLarge: Widget {
@@ -38,6 +66,11 @@ struct MyProvider3: TimelineProvider {
         let timeline = Timeline(entries: [model], policy: .atEnd)
         completion(timeline)
     }
+    internal func reloadTimeline() {
+            let newModel = MyModel3(date: Date(), imageURL: MapboxImageURLGenerator3.generateURL())
+            let timeline = Timeline(entries: [newModel], policy: .atEnd)
+            WidgetCenter.shared.reloadTimelines(ofKind: "Widget")
+        }
 }
 struct MyModel3: TimelineEntry {
     let date: Date
@@ -52,10 +85,7 @@ struct MyWidgetView3: View {
     var entry: MyProvider3.Entry
     
     
-    let locations = [
-        Location(name: "Kolkata", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-        //           Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
-    ]
+    @StateObject private var viewModel = WidgetViewModel2()
     
     let pinColor = Color(.white)
     
@@ -71,13 +101,13 @@ struct MyWidgetView3: View {
             }
             VStack {
                 
-                ForEach(locations) { location in
+                ForEach(viewModel.locations) { location in
                     
-                    HStack{
-                        Spacer()
+                    GeometryReader { geometry in
+                        
                         VStack {
                             
-                            Image("person3")
+                            Image(location.imageName)
                                 .resizable()
                                 .scaledToFill()
                                 .frame(width: 60, height: 60)
@@ -93,39 +123,85 @@ struct MyWidgetView3: View {
                                 .offset(y: -9)
                             
                             
-                        }
+                        }.offset(x: CGFloat.random(in: 0...(geometry.size.width - 60)),
+                                 y: CGFloat.random(in: 0...(geometry.size.height - 60)))
                     }
-                    .padding(.trailing, 12.0)
+                }
                     
+                  
                     HStack{
                         
-                        VStack{
-                            Text("Explore")
-                                .multilineTextAlignment(.leading)
-                                .fontWeight(.bold)
-                            Text(location.name)
-                                .multilineTextAlignment(.leading)
+                VStack {
+                            // Elevated Container
+            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color.white)
+                                .frame(height: 75)
+                                .padding()
+                                .overlay(
+                                HStack(spacing: 20) {
+                                        // Circle Button 1
+    CircleButton(imageName: "magnifyingglass", buttonColor: Color.gray, action: {
+                                            // Action for Button 1
+                                            print("Button 1 tapped")
+                                        })
+                                        
+                                        // Circle Button 2
+        CircleButton(imageName: "house", buttonColor: Color.purple, action: {
+                                            // Action for Button 2
+                                            print("Button 2 tapped")
+                                        })
+                                        
+                                        // Circle Button 3
+        CircleButton(imageName: "message", buttonColor: Color.green,action: {
+                                            // Action for Button 3
+                                            print("Button 3 tapped")
+                                        })
+                                        
+                                        // Circle Button 4
+        CircleButton(imageName: "gear", buttonColor: Color.orange, action: {
+                                            // Action for Button 4
+                                            print("Button 4 tapped")
+                                        })
+                                    }
+                                        
+                                ) .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
                         }
-                        Spacer()
                     }
-                    .padding(.leading, 8.0)
-                }
+                   
+                
             }
             //MapSnapshotImageView()
         }
     }
 }
+struct CircleButton: View {
+    let imageName: String
+    let buttonColor: Color
+    let action: () -> Void
+   
+    
+    var body: some View {
+        Button(action: action) {
+            Image(systemName: imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 35, height: 35)
+                .foregroundColor(.white)
+                .padding(8)
+                .background(Circle().fill(buttonColor))
+        }
+    }
+}
 
 struct MapboxImageURLGenerator3{
-    @State  var longitude = -0.141
-    @State var latitude = 51.501
+    
     
     static func generateURL() -> URL? {
         @State  var longitude = -0.141
         @State var latitude = 51.501
         // Replace this with your logic to generate Mapbox image URL
         return URL(string:
-        "https://api.mapbox.com/styles/v1/erklabs/clrktvr2v002q01peameb9am0/static/\(longitude),\(latitude),13,0/320x340@2x?access_token=pk.eyJ1IjoiZXJrbGFicyIsImEiOiJjbGtqcWRnYTMwbjc4M21sbml3eTUxbHZzIn0.xpSDMZkBifhBOyNdzu21Xw")
+                    "https://api.mapbox.com/styles/v1/erklabs/clrktvr2v002q01peameb9am0/static/\(longitude),\(latitude),13,0/320x340@2x?access_token=pk.eyJ1IjoiZXJrbGFicyIsImEiOiJjbGtqcWRnYTMwbjc4M21sbml3eTUxbHZzIn0.xpSDMZkBifhBOyNdzu21Xw")
     }
 }
 
